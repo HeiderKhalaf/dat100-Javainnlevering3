18475b898488f6d3f8c4df7fbc7156155fc037fe
HeiderKhalaf <184900@stud.hvl.no>
2024-11-11 14:55:50 +0100

    Update SkrivBlogg.java

--- src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
@@ -1,6 +1,8 @@
 package no.hvl.dat100.oppgave4;
 
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 
 import no.hvl.dat100.common.TODO;
@@ -9,7 +11,17 @@ import no.hvl.dat100.oppgave3.*;
 public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
-
-		throw new UnsupportedOperationException(TODO.method());
+		
+		String filsti = mappe + "/" + filnavn;
+		
+		try (PrintWriter writer = new PrintWriter(new FileWriter(filsti))) {
+			writer.print(samling.toString());
+			return true;
+		}catch (IOException e) {
+			System.out.println("En feil oppsto under skriving til filen: " + e.getMessage());
+			return false;
+		}
+		
+		
 	}
 }
